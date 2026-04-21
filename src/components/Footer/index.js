5bc6b7e25d3506c78a17ecb28cd447c289f8b278
Nikhil 'Nick' Sharma <163467746+Nick2543@users.noreply.github.com>
2026-04-20 21:14:42 -0700

    Update index.js

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -99,7 +99,7 @@ function Footer() {
           <SocialMediaIcon href={Bio.github} target="_blank"><GitHubIcon /></SocialMediaIcon>
         </SocialMediaIcons>
         <Copyright>
-          &copy; 2025 Nikhil 'Nick' Sharma. All rights reserved.
+          &copy; 2026 Nikhil 'Nick' Sharma. All rights reserved.
         </Copyright>
 
       </FooterWrapper>
